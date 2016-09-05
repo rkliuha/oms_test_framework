f305a060b9c218200033ab2b8f84976bb4013019
kmarko <marko.kvitoslava@gmail.com>
2016-09-05 20:22:57 +0300

    Maven Surefire added to pom.xml, TestCases package with test class created, testng.xml is used as path to tests

---
+++ src/main/java/academy/softserve/edu/TestCases/TestCaseForTest.java
@@ -0,0 +1,36 @@
+package academy.softserve.edu.TestCases;
+
+import org.openqa.selenium.*;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+public class TestCaseForTest {
+
+    final WebDriver driver = new FirefoxDriver();
+
+    @BeforeTest
+    final public void setUp() {
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+    }
+    @AfterTest
+    final public void teatDown() {
+        driver.close();
+    }
+
+    @Test
+    final public void testWebdriverPractice() {
+
+        driver.navigate().to("https://www.google.com");
+
+        driver.findElement(By.xpath("//input[@id='lst-ib']")).sendKeys("funny picture");
+
+        final String foundText = driver.findElement(By.xpath("//a[text()='Crazy Funny Pictures']")).getText();
+        Assert.assertTrue(foundText.toLowerCase().contains("funny picture"));
+    }
+}
