e0ce111e247182eb9cb4c15311b78e7f24cbc92c
Nikodych <shkliar.dmytro@gmail.com>
2016-09-07 13:54:49 +0300

    IFATQCZZ-67 - added listener class to make screenshot on test failure

---
+++ src/test/java/academy/softserve/edu/utils/TestListener.java
@@ -0,0 +1,32 @@
+package academy.softserve.edu.utils;
+
+import academy.softserve.edu.tests.TestClassForListener;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.testng.ITestResult;
+import org.testng.TestListenerAdapter;
+
+import java.io.File;
+import java.io.IOException;
+
+public class TestListener extends TestListenerAdapter {
+
+    @Override
+    public void onTestFailure(final ITestResult iTestResult) {
+        takeScreenShotOnFailure(iTestResult);
+        System.out.println("TestClass has failed: " + iTestResult.getName());
+    }
+
+    public void takeScreenShotOnFailure(final ITestResult iTestResult) {
+        final Object object = iTestResult.getInstance();
+        final WebDriver driver = ((TestClassForListener) object).getDriver();
+        final File screenShotFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+        try {
+            FileUtils.copyFile(screenShotFile, new File("screenshot/" + iTestResult.getName() + ".png"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
