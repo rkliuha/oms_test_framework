2cbeb01782a2e8500c81eb99527d451160c2374e
rkliuha <ngnaga2013@yandex.ua>
2016-09-07 19:06:54 +0300

    IFATQCZZ-26 - Improved Page Object Structure

--- src/test/java/academy/softserve/edu/pageobjects/UserInfoPage.java
+++ src/test/java/academy/softserve/edu/pageobjects/UserInfoPage.java
@@ -5,11 +5,13 @@ import org.openqa.selenium.WebDriver;
 
 public class UserInfoPage extends PageObject {
 
-    public static final By ADMINISTRATOR_PAGE =
-            By.xpath("//ul[@id='nav']//a[1]");
-    public static final By ORDERING_PAGE =
+    public static final By ADMINISTRATION_PAGE =
             By.xpath("//ul[@id='nav']//a[1]");
     public static final By ITEM_MANAGEMENT_PAGE =
+            By.xpath("//ul[@id='nav']//a[1]");
+    public static final By CUSTOMER_ORDERING_PAGE =
+            By.xpath("//ul[@id='nav']//a[1]");
+    public static final By MERCHANDISER_ORDERING_PAGE =
             By.xpath("//ul[@id='nav']//a[1]");
     public static final String USER_INFO_PAGE_URL =
             "http://192.168.56.101:8080/oms5/userInfo.htm";
@@ -18,19 +20,24 @@ public class UserInfoPage extends PageObject {
         super(driver);
     }
 
-    public final AdministrationPage clickAdministratorTab() {
-        driver.findElement(ADMINISTRATOR_PAGE).click();
+    public final AdministrationPage clickAdministrationTab() {
+        driver.findElement(ADMINISTRATION_PAGE).click();
         return new AdministrationPage(driver);
     }
 
-    public final CustomerOrderingPage clickOrderingTab() {
-        driver.findElement(ORDERING_PAGE).click();
+    public final CustomerOrderingPage clickCustomerOrderingTab() {
+        driver.findElement(CUSTOMER_ORDERING_PAGE).click();
         return new CustomerOrderingPage(driver);
     }
 
     public final ItemManagementPage clickItemManagementTab() {
         driver.findElement(ITEM_MANAGEMENT_PAGE).click();
         return new ItemManagementPage(driver);
+    }
+
+    public final MerchandiserOrderingPage clickMerchandiserOrderingTab() {
+        driver.findElement(MERCHANDISER_ORDERING_PAGE).click();
+        return new MerchandiserOrderingPage(driver);
     }
 
 }
