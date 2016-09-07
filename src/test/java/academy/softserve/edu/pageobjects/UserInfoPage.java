b1b4fa2b53ee8f46bd887c0c24240e774aadbd57
Roman Kliuha <ngnaga2013@yandex.ua>
2016-09-07 22:05:35 +0300

    IFATQCZZ-26 - Improved Locators

--- src/test/java/academy/softserve/edu/pageobjects/UserInfoPage.java
+++ src/test/java/academy/softserve/edu/pageobjects/UserInfoPage.java
@@ -5,13 +5,15 @@ import org.openqa.selenium.WebDriver;
 
 public class UserInfoPage extends PageObject {
 
-    public static final By ADMINISTRATION_PAGE =
-            By.xpath("//ul[@id='nav']//a[1]");
-    public static final By ITEM_MANAGEMENT_PAGE =
-            By.xpath("//ul[@id='nav']//a[1]");
-    public static final By CUSTOMER_ORDERING_PAGE =
-            By.xpath("//ul[@id='nav']//a[1]");
-    public static final By MERCHANDISER_ORDERING_PAGE =
+    public static final By ADMINISTRATION_LINK =
+            By.xpath("//ul[@id='nav']/descendant::a[@href='users.htm']");
+    public static final By ITEM_MANAGEMENT_LINK =
+            By.xpath("//ul[@id='nav']/descendant::a[@href='itemManagement.htm']");
+    public static final By CUSTOMER_ORDERING_LINK =
+            By.xpath("//ul[@id='nav']/descendant::a[@href='order.htm']");
+    public static final By MERCHANDISER_ORDERING_LINK =
+            By.xpath("//ul[@id='nav']/descendant::a[@href='order.htm']");
+    public static final By VISITOR_EXTRA_LINK =
             By.xpath("//ul[@id='nav']//a[1]");
     public static final String USER_INFO_PAGE_URL =
             "http://192.168.56.101:8080/oms5/userInfo.htm";
@@ -21,22 +23,22 @@ public class UserInfoPage extends PageObject {
     }
 
     public final AdministrationPage clickAdministrationTab() {
-        driver.findElement(ADMINISTRATION_PAGE).click();
+        driver.findElement(ADMINISTRATION_LINK).click();
         return new AdministrationPage(driver);
     }
 
     public final CustomerOrderingPage clickCustomerOrderingTab() {
-        driver.findElement(CUSTOMER_ORDERING_PAGE).click();
+        driver.findElement(CUSTOMER_ORDERING_LINK).click();
         return new CustomerOrderingPage(driver);
     }
 
     public final ItemManagementPage clickItemManagementTab() {
-        driver.findElement(ITEM_MANAGEMENT_PAGE).click();
+        driver.findElement(ITEM_MANAGEMENT_LINK).click();
         return new ItemManagementPage(driver);
     }
 
     public final MerchandiserOrderingPage clickMerchandiserOrderingTab() {
-        driver.findElement(MERCHANDISER_ORDERING_PAGE).click();
+        driver.findElement(MERCHANDISER_ORDERING_LINK).click();
         return new MerchandiserOrderingPage(driver);
     }
 
