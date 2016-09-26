5760550542fb97b6643eeb7d6b9f3f6be6de79bc
DmytroKuzin <dimabeton@mail.ru>
2016-09-27 01:37:07 +0300

    Ticket#68

--- src/test/java/academy/softserve/edu/utils/DataProviders.java
+++ src/test/java/academy/softserve/edu/utils/DataProviders.java
@@ -3,10 +3,14 @@ package academy.softserve.edu.utils;
 import academy.softserve.edu.enums.Roles;
 import org.testng.annotations.DataProvider;
 
+import java.io.IOException;
+
 import static academy.softserve.edu.pageobjects.CustomerOrderingPage.SHOW_10_ITEMS_LINK;
 import static academy.softserve.edu.pageobjects.CustomerOrderingPage.SHOW_5_ITEMS_LINK;
 
 public class DataProviders {
+
+    private static final String EXCEL_FILE = PropertiesReader.getDefaultProperty("EXCEL_FILE");
 
     @DataProvider
     static public Object[][] testDataForAllUsersRole() {
@@ -38,4 +42,24 @@ public class DataProviders {
         };
     }
 
+    @DataProvider
+    static final public Object[][] excelLogInDataForAllUsersRole() throws IOException {
+        return ExcelReader
+                .getArrayFromExcel(EXCEL_FILE, "sheet1");
+    }
+
+
+    @DataProvider
+    static final public Object[][] oneColumnOfExcelLoginData() throws IOException {
+        return ExcelReader
+                .getColumnByName(EXCEL_FILE, "password", "sheet1");
+    }
+
+
+    @DataProvider
+    static final public Object[][] oneColumnFromDefaultExcelFile() throws IOException {
+        return ExcelReader
+                .getColumnByName("password", "sheet1");
+    }
 }
+
