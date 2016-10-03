3be7bf3e5bbb0d4b01ff7b9be199144bfbb22988
kmarko <marko.kvitoslava@gmail.com>
2016-10-03 20:02:31 +0300

    IFATQCZZ-12 Added As a Merchansider I want to delete some particular order from the list of existing orders

--- src/test/java/academy/softserve/edu/elements/locators/merchandiser/MerchandiserOrderingPageLocators.java
+++ src/test/java/academy/softserve/edu/elements/locators/merchandiser/MerchandiserOrderingPageLocators.java
@@ -38,29 +38,57 @@ public enum MerchandiserOrderingPageLocators implements ILocator {
             LocatorsType.BY_XPATH,
             "//div[@id='list']/table//th[1]/a"),
 
+    ORDER_NAME_CELL("Order Name Cell",
+            LocatorsType.BY_XPATH,
+            "//div[@id='list']//tr[%s]/td[1]"),
+
     TOTAL_PRICE_HEADER_LINK("Total Price Header Link",
             LocatorsType.BY_XPATH,
             "//div[@id='list']/table//th[2]/a"),
+
+    TOTAL_PRICE_CELL("Total Price Cell",
+            LocatorsType.BY_XPATH,
+            "//div[@id='list']//tr[%s]/td[2]"),
 
     MAX_DISCOUNT_HEADER_LINK("Max Discount Header Link",
             LocatorsType.BY_XPATH,
             "//div[@id='list']/table//th[3]/a"),
 
+    MAX_DISCOUNT_CELL("Max Discount Cell",
+            LocatorsType.BY_XPATH,
+            "//div[@id='list']//tr[%s]/td[3]"),
+
     DELIVERY_DATE_HEADER_LINK("Delivery Date Header Link",
             LocatorsType.BY_XPATH,
             "//div[@id='list']/table//th[4]/a"),
+
+    DEELIVERY_DATE_CELL("Delivery Date Cell",
+            LocatorsType.BY_XPATH,
+            "//div[@id='list']//tr[%s]/td[4]"),
 
     STATUS_HEADER_LINK("Status Header Link",
             LocatorsType.BY_XPATH,
             "//div[@id='list']/table//th[5]/a"),
 
+    STATUS_CELL("Status Cell",
+            LocatorsType.BY_XPATH,
+            "//div[@id='list']//tr[%s]/td[5]"),
+
     EDIT_HEADER("Edit Header",
             LocatorsType.BY_XPATH,
             "//div[@id='list']//th[6]"),
 
+    EDIT_CELL_LINK("Edit Cell Link",
+            LocatorsType.BY_XPATH,
+            "//div[@id='list']//tr[%s]/td[6]/a"),
+
     DELETE_HEADER("Delete Header",
             LocatorsType.BY_XPATH,
             "//div[@id='list']//th[7]"),
+
+    DELETE_CELL_LINK("Delete Cell Link",
+            LocatorsType.BY_XPATH,
+            "//div[@id='list']//tr[%s]/td[7]"),
 
     FIRST_NAVIGATION_BUTTON("First Navigation Button",
             LocatorsType.BY_ID,
@@ -89,6 +117,7 @@ public enum MerchandiserOrderingPageLocators implements ILocator {
     PAGE_COUNT_TEXT("Page Count Text",
             LocatorsType.BY_ID,
             "pageCount");
+
 
     protected String elementName;
     protected LocatorsType locatorType;
